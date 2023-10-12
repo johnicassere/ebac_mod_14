3000fa1f024a5326be051ccbf65591b370e10c50
unknown <johnicassere@hotmail.com>
2023-10-12 17:43:46 -0300

    Add jquery Validation

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ebac_mod_14
 
 Desafio:
- aplicar mascara para o telefone e aplicar validação atravez do jquery validation 
+ aplicar mascara para o telefone e aplicar validação atravez do jquery validation.
+
+ obs: No plugin os numeros são representados por "0" zero e 
+ letras são representadas pela letra "s". 
--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+
+
 $(document).ready(function(){
-    $('#telefone').mask('(00) 0 0000 - 0000', {
-        placeholder: '(00) 0 0000 - 0000'
+let mascara = '(00) 0000-0000'
+
+
+    $('#telefone').mask(mascara, {   
+       placeholder: '(00) 0000-0000'
+    })
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required: true,
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            telefone:{
+                required: true,
+            },
+            mensagem:{
+                required: true,
+            }
+        },
+        messages:{
+            nome: 'Por favor digitar o seu nome',
+            email: 'Por favor digitar o seu email',
+            mensagem: 'Pro favor digite sua mensagem',
+            telefone: 'Por favor digitar o seu telefone'
+        },
+        submitHandler:function(form){
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador){
+            let camposInsert = validador.numberOfInvalids();
+            if(camposInsert){
+                alert(`Existem ${camposInsert} campos inconrretos`)
+            }
+        }
+
     })
 })
     
